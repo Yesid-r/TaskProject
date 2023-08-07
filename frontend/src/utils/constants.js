222deb8b886d02d9d1d941715dae0fec680daada
Yesid Rincon <dairo.rincon@uptc.edu.co>
2023-08-07 18:29:46 -0500

    add url deploy

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,3 @@
 export const TITLE = "Project task manager";
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://task-project-seven.vercel.app";
